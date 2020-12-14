dfd29105c43745969b7e2071c10078c73d1d7c49
Dmitry Kovalenko <dmytro.kovalenko@ironsrc.com>
2020-12-14 23:58:28 +0200

    Add timeout option for Prometheus provider

--- gdbt/provider/prometheus.py
+++ gdbt/provider/prometheus.py
@@ -1,27 +1,37 @@
-from typing import Any, List
+import typing
 
 import attr
 import deserialize  # type: ignore
+import durations  # type: ignore
 import requests
 
 from gdbt.provider.provider import EvaluationProvider, Provider
 
-HTTP_TIMEOUT = 1.0
+HTTP_TIMEOUT = 5.0
+
+
+def convert_duration(duration_raw: typing.Union[str, int, float]) -> float:
+    duration_str = str(duration_raw)
+    if duration_str.isdigit():
+        duration_str += "s"
+    duration = durations.Duration(duration_str).to_seconds()
+    return duration
 
 
 @deserialize.downcast_identifier(Provider, "prometheus")
 @attr.s
 class PrometheusProvider(EvaluationProvider):
-    endpoint: str
+    endpoint: str = attr.ib()
+    timeout: float = attr.ib(default=HTTP_TIMEOUT, converter=convert_duration)
 
     @property
     def client(self) -> None:
         return None
 
-    def query(self, query: str) -> List[Any]:
+    def query(self, query: str) -> typing.List[typing.Any]:
         url = self.endpoint.rstrip("/") + "/api/v1/query"
         params = {"query": query}
-        response = requests.get(url, params, timeout=HTTP_TIMEOUT)
+        response = requests.get(url, params, timeout=self.timeout)
         response.raise_for_status()
         answer = response.json().get("data").get("result")
         return answer
